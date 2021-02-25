449e6ed767d4ab111636e7b44040af8acfbef627
George Owen <george.owen@asurion.com>
2021-02-25 07:40:22 -0600

    Directory cleanup and rename Last20 dir to Last100 dir

--- getlast100.sh
+++ getlast100.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 #stamp=`date +%s`
 stamp="out"
-namebase="last20"
+namebase="last100"
 namefull="${namebase}-${stamp}.json"
+BASEDIR="/Users/george.owen/src/GitHub/EinhornWatch/Last100"
 #sender_id="73205709" #Einhorn
 #sender_id="47126376"
 #after_id="161376602848318931"
-after_id=`cat /Users/george.owen/src/GitHub/EinhornWatch/Last20/latestID.txt`
+after_id=`cat $BASEDIR/latestID.txt`
+old_latest_text=`cat $BASEDIR/latestText.txt`
 urlbase='https://api.groupme.com/v3/groups/32841593/messages?token=5u7VXqv9CBploHRLSoasGFAUyAsUWfhHBWH2zZBd&limit=100&after_id='
 urlmessid="${after_id}"
 url="${urlbase}${urlmessid}"
@@ -16,44 +18,56 @@ url="${urlbase}${urlmessid}"
 #else;
 #fi;
 
-#curl --location --request GET "https://api.groupme.com/v3/groups/32841593/messages?token=5u7VXqv9CBploHRLSoasGFAUyAsUWfhHBWH2zZBd&limit=100&after_id=161376602848318931" >> /Users/george.owen/src/GitHub/EinhornWatch/Last20/$namefull;
-curl --location --request GET "${url}" > /Users/george.owen/src/GitHub/EinhornWatch/Last20/$namefull; 
+#curl --location --request GET "https://api.groupme.com/v3/groups/32841593/messages?token=5u7VXqv9CBploHRLSoasGFAUyAsUWfhHBWH2zZBd&limit=100&after_id=161376602848318931" >> $BASEDIR/$namefull;
+curl --location --request GET "${url}" > $BASEDIR/$namefull; 
 	
 
 #formats output for jq parsing
-sed -ig 's/"response":{//g' /Users/george.owen/src/GitHub/EinhornWatch/Last20/$namefull
-sed -ig 's/"count":[0-9][0-9][0-9][0-9][0-9][0-9],//g' /Users/george.owen/src/GitHub/EinhornWatch/Last20/$namefull
-sed -ig 's/},"meta":{"code":200}//g' /Users/george.owen/src/GitHub/EinhornWatch/Last20/$namefull
-rm /Users/george.owen/src/GitHub/EinhornWatch/Last20/*.jsong
+sed -ig 's/"response":{//g' $BASEDIR/$namefull
+sed -ig 's/"count":[0-9][0-9][0-9][0-9][0-9][0-9],//g' $BASEDIR/$namefull
+sed -ig 's/},"meta":{"code":200}//g' $BASEDIR/$namefull
+rm $BASEDIR/*.jsong
 
 #output einhorn message text in last n messages
-cat /Users/george.owen/src/GitHub/EinhornWatch/Last20/$namefull | jq --unbuffered '.messages[] | select(.sender_id == "73205709") | .text' | tee /Users/george.owen/src/GitHub/EinhornWatch/Last20/jqout.text.txt
-cat /Users/george.owen/src/GitHub/EinhornWatch/Last20/$namefull | jq --unbuffered '.messages[] | select(.sender_id == "73205709") | .id' | tee /Users/george.owen/src/GitHub/EinhornWatch/Last20/jqout.messageid.txt
+cat $BASEDIR/$namefull | jq --unbuffered '.messages[] | select(.sender_id == "73205709") | .text' | tee $BASEDIR/jqout.text.txt
+cat $BASEDIR/$namefull | jq --unbuffered '.messages[] | select(.sender_id == "73205709") | .id' | tee $BASEDIR/jqout.messageid.txt
 
 #remove "" from message id out file
-sed -ig 's/"//g' /Users/george.owen/src/GitHub/EinhornWatch/Last20/jqout.messageid.txt
-rm /Users/george.owen/src/GitHub/EinhornWatch/Last20/*.txtg
+sed -ig 's/"//g' $BASEDIR/jqout.messageid.txt
+rm $BASEDIR/*.txtg
 
 #gets message id of latest Einhorn messahe
-latest_id=`awk '/./{line=$0} END{print line}' /Users/george.owen/src/GitHub/EinhornWatch//Last20/jqout.messageid.txt`
+#WRONG: Gets last line (oldest message ID) #latest_id=`awk '/./{line=$0} END{print line}' /Users/george.owen/src/GitHub/EinhornWatch//Last20/jqout.messageid.txt`
+latest_id=`awk 'NR==1' $BASEDIR/jqout.messageid.txt`
+latest_text=`awk 'NR==1' $BASEDIR/jqout.text.txt`
+unread_texts=`cat $BASEDIR/jqout.text.txt`
 
 if [ -z $latest_id ]
 then
-  echo "\nNo new messages since message ID: $after_id"
-  echo "Writing previous message ID (above) to LatestID.txt"
-  echo $after_id > /Users/george.owen/src/GitHub/EinhornWatch/Last20/latestID.txt
+#  echo $unread_texts > $BASEDIR/UnreadTexts.txt
+  printf "$unread_texts" > $BASEDIR/UnreadTexts.txt
   latest_id=$after_id
+  latest_text=$old_latest_text
+  echo $old_latest_text > $BASEDIR/latestText.txt
+  echo $after_id > $BASEDIR/latestID.txt
+  echo "\nNo new messages since:\nID: $after_id\nText: $old_latest_text"
+  echo "Writing previous message ID (above) to LatestID.txt"
 else
-  echo $latest_id > /Users/george.owen/src/GitHub/EinhornWatch/Last20/latestID.txt
+  echo $unread_texts > $BASEDIR/UnreadTexts.txt
+  echo $latest_id > $BASEDIR/latestID.txt
+  echo $latest_text > $BASEDIR/latestText.txt
 fi
 
-#echo $latest_id > /Users/george.owen/src/GitHub/EinhornWatch/Last20/latestID.txt
+#echo $latest_id > $BASEDIR/latestID.txt
+#echo $latest_text > $BASEDIR/latestText.txt
 
 #CLI Outputs for Validation
-echo "\nafter_id (in URL): $after_id"
+echo "\n------------------\nafter_id (in URL): $after_id"
 echo "latest_id: $latest_id"
+echo "old latest text: $old_latest_text"
+echo "latest text: $latest_text"
 echo "pulled URL: $urlbase$after_id"
 echo "next URL:   $urlbase$latest_id"
-#cat /Users/george.owen/src/GitHub/EinhornWatch/Last20/latestID.txt
+#cat $BASEDIR/latestID.txt
 #echo $urlmessid
 #echo $url
